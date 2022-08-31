fc6b34aeff5fc41f773ab612cf232903bb34870f
NikunjBalar <kunj.hasi103@gmail.com>
2022-08-31 10:31:07 +0530

    program for datatype

---
+++ DataType/Details.java
@@ -0,0 +1,27 @@
+class Nikunj {
+	
+	static long moNO ;
+	static boolean business;
+	static char dChar =' ';
+	public static void main( String [] nik ){
+
+		System.out.println("Default value for char"+dChar+"is");
+		System.out.println("Default value for boolean =" + business);
+		System.out.println( moNO );
+		System.out.println( dChar );
+		System.out.println("Starting of main method");
+		byte age = 26 ;
+		System.out.println( age );
+		char middleName = 'p' ;
+		System.out.println( middleName );
+		boolean isMerrid = false ;
+		System.out.println( isMerrid );
+		float aggrregate = 87.4535f;
+		double percentage = 232341.32423;
+		System.out.println( age + middleName  );
+	}
+
+
+}
+
+
